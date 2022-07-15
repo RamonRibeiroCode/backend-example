37e6ea756596ec50591291bb70fa192b043bc5f5
Ramon Ribeiro <ramon_ramos_ext@carrefour.com>
2022-07-14 22:35:39 -0300

    feat: cria teste para criacao de usuario

---
+++ src/modules/user/repositories/in-memory/UsersRepositoryInMemory.ts
@@ -0,0 +1,39 @@
+import { ICreateUserDTO } from '@modules/user/dtos/ICreateUserDTO'
+import { User } from '@modules/user/infra/typeorm/entities/User'
+import { IUsersRepository } from '../IUsersRepository'
+
+class UsersRepositoryInMemory implements IUsersRepository {
+  users: User[] = []
+
+  async create (data: ICreateUserDTO): Promise<User> {
+    const user = new User()
+
+    Object.assign(user, {
+      ...data,
+      id: Math.random()
+    })
+    this.users.push(user)
+
+    return user
+  }
+
+  async findByEmail (email: string): Promise<User> {
+    const user = this.users.find(user => user.email === email)
+
+    return user
+  }
+
+  async findById (id: number): Promise<User> {
+    const user = this.users.find(user => user.id === id)
+
+    return user
+  }
+
+  async save (user: User): Promise<User> {
+    this.users.push(user)
+
+    return user
+  }
+}
+
+export { UsersRepositoryInMemory }
---
+++ src/modules/user/useCases/createUser/CreateUserUseCase.spec.ts
@@ -0,0 +1,26 @@
+import { UsersRepositoryInMemory } from '@modules/user/repositories/in-memory/UsersRepositoryInMemory'
+import { CreateUserUseCase } from './CreateUserUseCase'
+
+let createUserUseCase: CreateUserUseCase
+let usersRepositoryInMemory: UsersRepositoryInMemory
+
+describe('Create User', () => {
+  beforeEach(() => {
+    usersRepositoryInMemory = new UsersRepositoryInMemory()
+    createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory)
+  })
+
+  it('should be able to create a new user', async () => {
+    const newUser = await createUserUseCase.execute({
+      email: 'email@example.com',
+      firstName: 'First Name',
+      lastName: 'Last Name',
+      age: 20,
+      password: '123456'
+    })
+
+    const userInMemory = await usersRepositoryInMemory.findByEmail(newUser.email)
+
+    expect(userInMemory).toHaveProperty('id')
+  })
+})
